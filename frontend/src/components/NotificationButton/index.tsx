1cf0b652f731d78589f22f7a77fb9fbbd9829247
Mauricio Ferreira <99357847+mferreiradb@users.noreply.github.com>
2022-09-06 22:41:05 -0300

    Add files via upload

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,10 @@
+import Icon from '../../assets/img/Vector.svg'
+import './style.css'
+
+export default function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={ Icon } alt="Notificar" />
+        </div>
+    )
+}
